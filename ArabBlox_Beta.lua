a73ab7d989f4e28035edced3afedb095d749d550
anyahubs <131607266+anyahubs@users.noreply.github.com>
2023-08-03 15:46:56 +0300

    Update ArabBlox_Beta.lua

--- ArabBlox_Beta.lua
+++ ArabBlox_Beta.lua
@@ -5,7 +5,7 @@
 repeat task.wait() until game:IsLoaded()
 repeat task.wait() until game:GetService('Players').LocalPlayer ~= nil
 loadstring(game:HttpGet('https://eternityhub.xyz/BetterRoblox/Core/MainCore.lua'))()
-wait(3)
+wait(30)
 
 
 
